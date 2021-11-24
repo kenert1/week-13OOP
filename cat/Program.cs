2b8aa3896ad5497c55cbfa437ab7a37014ae70f0
Opilane TTHK <opilane@tthk.ee>
2021-11-24 13:05:07 +0200

    first commit

---
+++ cat/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
